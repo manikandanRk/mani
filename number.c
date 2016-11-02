76f33c1e96077e0ba7f21f0efe039a8b972b9d48
manikandanRk <mail4mani97@gmail.com>
2016-11-02 06:19:47 -0700

    Add files via upload

---
+++ number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number;
+scanf("%d",&number);
+if(n<0)
+printf("Positive");
+else if(n<0)
+printf("Negative");
+else
+printf("zero");
+getch();
+}
